61ab4c714567f9ece87bba6ba89b96b935e23d01
Naif1995 <nkhnain@hotmail.com>
2022-01-23 23:07:27 +0300

    add placeHolder

--- src/main/java/com/family/chat/familChat/controller/WebSocketController.java
+++ src/main/java/com/family/chat/familChat/controller/WebSocketController.java
@@ -1,9 +1,11 @@
 package com.family.chat.familChat.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class WebSocketController {
@@ -15,10 +17,9 @@ public class WebSocketController {
         this.template = template;
     }
 
-    @MessageMapping("/send/message/{id}")
-    public void sendMessage(String message) {
-        System.out.println("Hi Every one");
-        System.out.println(message);
-        this.template.convertAndSend("/message", message);
+    @MessageMapping("/send/message/{chatName}")
+    public void sendMessage(@DestinationVariable String chatName) {
+        System.out.println(chatName);
+        this.template.convertAndSend("/message/"+chatName, "Welcome to "+chatName);
     }
 }
