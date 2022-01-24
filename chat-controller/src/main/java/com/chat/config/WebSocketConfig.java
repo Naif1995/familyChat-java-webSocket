5f0f01781527f26473a8ed2b423d32cbebb185d1
Naif bin khunayn <nkhnain@hotmail.com>
2022-01-24 13:07:21 +0300

    refactor the structure

--- chat-controller/src/main/java/com/chat/config/WebSocketConfig.java
+++ chat-controller/src/main/java/com/chat/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.family.chat.familChat.config;
+package com.chat.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
--- chat-controller/src/main/java/com/chat/controller/WebSocketController.java
+++ chat-controller/src/main/java/com/chat/controller/WebSocketController.java
@@ -1,8 +1,9 @@
-package com.family.chat.familChat.controller;
+package com.chat.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,16 +11,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class WebSocketController {
 
-    private final SimpMessagingTemplate template;
 
     @Autowired
-    WebSocketController(SimpMessagingTemplate template) {
-        this.template = template;
-    }
+    SimpMessagingTemplate template;
 
     @MessageMapping("/send/message/{chatName}")
-    public void sendMessage(@DestinationVariable String chatName) {
-        System.out.println(chatName);
-        this.template.convertAndSend("/message/"+chatName, "Welcome to "+chatName);
+    public void sendMessage(@Payload String body, @DestinationVariable String chatName) {
+        System.out.println(body);
+        template.convertAndSend("/message/"+chatName, "Welcome to "+chatName);
     }
 }
