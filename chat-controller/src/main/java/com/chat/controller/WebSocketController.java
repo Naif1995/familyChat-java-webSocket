618996a3f82d7759e75495e4e04a773ea2a7d5b1
Naif bin khunayn <nkhnain@hotmail.com>
2022-01-25 08:27:46 +0300

    fix heroku deploy

--- chat-controller/src/main/java/com/chat/controller/WebSocketController.java
+++ chat-controller/src/main/java/com/chat/controller/WebSocketController.java
@@ -1,13 +1,12 @@
 package com.chat.controller;
 
+import com.chat.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import services.ChatService;
 
 @Controller
 public class WebSocketController {
